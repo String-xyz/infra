202e39707212b30c0ab30c0ac7c051a9d2b7196c
Marlon Monroy <saito.sv.h@gmail.com>
2022-06-10 13:20:28 -0700

    hot wallet secret

--- development/ssm/main.tf
+++ development/ssm/main.tf
@@ -4,18 +4,17 @@ locals {
 
 variable "hot_wallet_value" {
   type = string
-
+  sensitive = true
 }
 
 resource "aws_secretsmanager_secret" "hot_wallet" {
   name = "hot-wallet"
+  tags = {
+    Environment = local.env
+  }
 }
 
 resource "aws_secretsmanager_secret_version" "hot_wallet" {
-  secret_id = aws_secretsmanager_secret.hot_wallet
+  secret_id = aws_secretsmanager_secret.hot_wallet.id
   secret_string = var.hot_wallet_value
-}
-
-data "aws_kms_key" "kms" {
-  key_id = "alias/main-kms-key"
 }
