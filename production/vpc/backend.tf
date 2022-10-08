bcd36078a91639f44277f96096bada152e8a1488
Marlon Monroy <saito.sv.h@gmail.com>
2022-10-07 21:50:02 -0700

    STR-162

---
+++ production/vpc/backend.tf
@@ -0,0 +1,25 @@
+locals {
+  state_bucket  = "prod-string-terraform-state"
+  vpc_state_key = "vpc.tfstate"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+   required_providers {
+    aws = { 
+      source = "hashicorp/aws"
+      version = "4.14.0"
+    }
+  }
+
+  backend "s3" {
+    encrypt        = true
+    key            = "vpc.tfstate"
+    bucket         = "prod-string-terraform-state"
+    dynamodb_table = "prod-string-terraform-state-lock"
+    region         = "us-west-2"
+  }
+}
