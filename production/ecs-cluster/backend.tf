ceac3a21a8be3559024a414608d35e4d32a8ff3c
saito <saito.sv.h@gmail.com>
2023-05-23 10:12:36 -0700

    prod cluster

---
+++ production/ecs-cluster/backend.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+   required_providers {
+    aws = { 
+      source = "hashicorp/aws"
+      version = "4.37.0"
+    }
+  }
+
+  backend "s3" {
+    encrypt        = true
+    key            = "ecs-cluster.tfstate"
+    bucket         = "prod-string-terraform-state"
+    dynamodb_table = "prod-string-terraform-state-lock"
+    region         = "us-west-2"
+  }
+}
+
